d406b6e9a374cda13685439daa5c3fd20063c6a4
Tamires Rodrigues Delmondes <89863897+tamiresrodrigue@users.noreply.github.com>
2024-10-29 21:20:33 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu nome:")
+var age = Number(prompt("Digite sua idade")) 
+var courso = prompt("Qual seu curso?")
+var profession = prompt("Qual sua profisão?")
+
+alert(`nome: ${nome}`)
